3995353e6c1f529266fdecc93dbf88961bc1bce5
KalBence <kalman.bence95@gmail.com>
2018-03-04 19:40:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,103 @@
+const electron = require('electron')
+const {Menu, shell, dialog, ipcMain} = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+let template = [{
+  label: 'Todos',
+  submenu: [{ 
+    label: 'New',
+    click: function (item, focusedWindow) {
+      if (focusedWindow) {
+
+        addWindow = new BrowserWindow({width: 400, height: 200})
+        addWindow.loadURL(url.format({
+          pathname: path.join(__dirname, 'newTodo.html'),
+          protocol: 'file:',
+          slashes: true
+        }))
+
+        addWindow.on('closed', function () {
+          addWindow = null
+        })
+      }
+    }
+  }]
+}, {
+  label: 'View',
+  submenu: [{
+    label: 'Toggle Developer Tools',
+    accelerator: (() => {
+      if (process.platform === 'darwin') {
+        return 'Alt+Command+I'
+      } else {
+        return 'Ctrl+Shift+I'
+      }
+    })(),
+    click: (item, focusedWindow) => {
+      if (focusedWindow) {
+        focusedWindow.toggleDevTools()
+      }
+    }
+  }]
+}]
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+ipcMain.on('new-todo', (event, arg) => {
+  mainWindow.webContents.send('new-todo', arg)
+})
